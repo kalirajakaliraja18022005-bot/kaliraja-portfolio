8a7800dde347a70b6e85c8166c500253a97876f9
kaliraja <kalirajakaliraja18022005@gmail.com>
2026-08-21 22:26:29 +0530

    Direct switch to SQLite database

--- backend/database.py
+++ backend/database.py
@@ -5,5 +5,5 @@ DB_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), "portfolio.db
 
 def get_connection():
     conn = sqlite3.connect(DB_FILE, check_same_thread=False)
-    conn.row_factory = sqlite3.Row  # Dictionary madhiri data access panna
+    conn.row_factory = sqlite3.Row
     return conn
